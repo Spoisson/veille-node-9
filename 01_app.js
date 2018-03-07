135b09c1c6f4a8e34cc3a9dcfb46b9f4d09a8343
spoisson <spoisson@hotmail.ca>
2018-03-07 17:21:09 -0500

    Fonction ajouter opérationnelle mais elle n'affiche pas la ligne qui a été ajouté en plus

--- 01_app.js
+++ 01_app.js
@@ -238,8 +238,8 @@ app.post('/ajax_ajouter', (req,res) => {
 	 db.collection('adresse').save(req.body, (err, result) => {
 	 if (err) return console.log(err)
 	 // console.log(req.body)	
-	 console.log('sauvegarder dans la BD')
-	 res.send(JSON.stringify(req.body));
+	 //console.log(result.ops[0]._id)
+	res.redirect('/adresse')
 
 	 })
 
